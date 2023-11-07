4b42c4af081a07afd5f72b5c91be9799ddbd41e7
colormotor <drand48@gmail.com>
2023-11-07 11:49:03 +0000

    adding standalone runner

---
+++ py5canvas/__init__.py
@@ -0,0 +1,8 @@
+import inspect
+from . import run_sketch
+
+def run():
+    frame = inspect.stack()[1]
+    module = inspect.getmodule(frame[0])
+    filename = module.__file__
+    run_sketch.main(filename, standalone=True)
