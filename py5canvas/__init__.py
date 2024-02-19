2ee34a5aa33a58475dd511ea33a09da3eeca2b1d
colormotor <drand48@gmail.com>
2024-02-19 15:09:27 +0000

    ... back

--- py5canvas/__init__.py
+++ py5canvas/__init__.py
@@ -6,6 +6,4 @@ def run():
     frame = inspect.stack()[1]
     module = inspect.getmodule(frame[0])
     filename = module.__file__
-    os.chdir(os.path.basename(filename))
-    print("Setting working directory to", os.path.basename(filename))
     run_sketch.main(filename, standalone=True)
