2ce95d6f72c1b9bccc1452dee09613673fbe48a1
colormotor <drand48@gmail.com>
2024-06-11 14:30:14 +0200

    update requirements

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='py5canvas',
-    version='0.1',
+    version='0.2',
     description='Library to create drawings and animations with an interface similar to p5js and Processing.',
     author='Daniel Berio',
     author_email='drand48@gmail.com',
     packages=['py5canvas'],
-    install_requires=['matplotlib', 'pycairo', 'pyglet', 'numpy'],
+    install_requires=['matplotlib', 'pycairo', 'pyglet', 'numpy', 'easydict', 'xdialog', 'imgui[pyglet]',
     entry_points={
         'console_scripts': [
             'py5sketch = py5canvas.run_sketch:main'
