c807bd568299c579a54e9ef84c6cc20bf1255d2d
colormotor <drand48@gmail.com>
2024-07-11 22:28:31 +0200

    fix error with numpy encoder

--- py5canvas/sketch_params.py
+++ py5canvas/sketch_params.py
@@ -42,15 +42,20 @@ import json, codecs, os, copy, shutil
 class NumpyEncoder(json.JSONEncoder):
     """ Special json encoder for numpy types """
     def default(self, obj):
-        if isinstance(obj, (np.int_, np.intc, np.intp, np.int8,
+        if isinstance(obj, (np.intc, np.intp, np.int8,
             np.int16, np.int32, np.int64, np.uint8,
             np.uint16, np.uint32, np.uint64)):
             return int(obj)
-        elif isinstance(obj, (np.float_, np.float16, np.float32,
+        elif isinstance(obj, (np.float16, np.float32,
             np.float64)):
             return float(obj)
         elif isinstance(obj,(np.ndarray,)): #### This is the fix
             return obj.tolist()
+        elif isinstance(obj, (np.bool)):
+            return bool(obj)
+        elif isinstance(obj, (np.complex128)):
+            return complex(bool)
+
         return json.JSONEncoder.default(self, obj)
 
 def load_json(path, encoder=NumpyEncoder):
