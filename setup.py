23bc429a70f1800aa5a74a41a8c0383fa9a2cd4d
colormotor <drand48@gmail.com>
2024-10-10 14:13:18 +0100

    bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 # twine upload dist/*
 setup(
     name='py5canvas',
-    version='0.4.4',
+    version='0.4.5',
     description='Library to create drawings and animations with an interface similar to p5js and Processing.',
     author='Daniel Berio',
     author_email='drand48@gmail.com',
